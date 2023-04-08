5942da2379d48094cd569b2f394c05934ff50b5e
Emily Asay <emilygailasay@gmail.com>
2023-04-08 15:28:18 -0600

    tweaks to nextRound btn

---
+++ mongoinsert.js
@@ -0,0 +1,25 @@
+
+const { MongoClient } = require('mongodb');
+
+const userName = 'holowaychuk';
+const password = 'express';
+const hostname = 'mongodb.com';
+
+const url = `mongodb+srv://emilygailasay:wXw7RcqGiZeCLkWv@startup.9qpc7no.mongodb.net/`;
+
+async function main() {
+    const client = new MongoClient(url);
+
+    const scoreCollection = await client.db('startup').collection('score').insertMany(data);
+
+console.log('Inserted ${scoreCollection.insertedCount} docs')
+
+scores = [
+    { name: 'Emily', score: 350 },
+    { name: 'Eli', score: 400 },
+];
+
+scoreCollection.insertOce(score[0]);
+scoreCollection.insertMany(scores);
+
+}
